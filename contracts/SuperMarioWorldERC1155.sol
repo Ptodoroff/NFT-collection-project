fb7e35fc6c5b615b15695c31dc676ccec489707c
Petar <p.todorov212@gmail.com>
2022-09-14 09:23:20 +0300

    created dummy metadata for deployment via the erc1155

---
+++ contracts/SuperMarioWorldERC1155.sol
@@ -0,0 +1,35 @@
+pragma solidity 0.8.9;
+
+import "./ERC1155.sol";
+
+contract SuperMarioWorldERC1155 is ERC1155 {
+    string public name;
+    string public symbol;
+    uint tokenCount;     //keeps track of the id that the nft will be given upon mint 
+
+    mapping (uint => string ) private _tokenURIs;
+
+    constructor (string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+    }
+
+    //gets the uri link for the metadata of the queried nft
+    function uri (uint tokenId) public view returns (string memory) {
+        return _tokenURIs[tokenId];
+    }
+
+    //mints the amount of the token with the corresponding id
+    function mint (uint amount, string memory _uri) public {
+        require (msg.sender != address(0), "Cannot mint to the zero address");
+        tokenCount +=1;
+        _balances[tokenCount][msg.sender] += amount;
+        _tokenURIs[tokenCount]= _uri;               // sets the uri that has been passed as an input as the uri of the corresponding token ID
+
+        emit  TransferSingle(msg.sender, (address(0)), msg.sender, tokenCount, amount);
+    }
+
+    function supportsInterface (bytes4 interfaceId) public pure override returns (bool){
+    return interfaceId==0xd9b67a86 || interfaceId == 0x0e89341c;        // the second interface Id checks if we support uri function that retrieves the uri and (consequently ) the metadata, which helps the marketplace to visualise the nft easier
+
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,14 +2,14 @@ const {ethers} = require ("hardhat");         // adding the ethers.js library pr
 
 async function main() {                       // deployment code is contained here
 
-  const SuperMarioWorld = await ethers.getContractFactory("SuperMarioWorldOZ");  // this ethers.js method gets my compiled contract that I will deploy
-  const superMarioWorld = await SuperMarioWorld.deploy("SuperMarioWorldOZ", "SPRMoz"); // this is deploying the contract. the arguments in the brackets are the one, required by the contract's constructor
+  const SuperMarioWorld = await ethers.getContractFactory("SuperMarioWorld1155");  // this ethers.js method gets my compiled contract that I will deploy
+  const superMarioWorld = await SuperMarioWorld.deploy("SuperMarioWorld1155", "SPRM1155"); // this is deploying the contract. the arguments in the brackets are the one, required by the contract's constructor
 
   await superMarioWorld.deployed();  //awaits for the contract to get deployed before doing anything further
 
   console.log("contract deployed to Mumbai at address:" + superMarioWorld.address);
 
-  await superMarioWorld.mint("https://ipfs.io/ipfs/QmQPM3QkQMCenuterQkRRJL224UYm5Pgf6mdHfAcTN1912")    // THIS MUST BE THE CID OF THE JSON FILE THAT CONTAINS THE METADATA !!!! I  directly invoke the mint function upon deployment. it accepts the IPFS link of the JSON file, containing the metadata of the NFT that i want to mint.
+  await superMarioWorld.mint(5,"https://ipfs.io/ipfs/QmRnQxh5xj99LgdRkJaSPGD6bX6dW4TZ47HsUq1Mw7pBWc")    // THIS MUST BE THE CID OF THE JSON FILE THAT CONTAINS THE METADATA !!!! I  directly invoke the mint function upon deployment. it accepts the IPFS link of the JSON file, containing the metadata of the NFT that i want to mint.
 
 
 }       //braces!
